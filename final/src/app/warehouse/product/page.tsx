9774317a9d0a39216ad14c4962f7914521bc0e13
wayne-yang7021 <Wayne.yang7021@gmail.com>
2023-12-25 12:22:09 +0800

    Revert "Merge remote-tracking branch 'origin/sophie'"
    This reverts commit 6b6df13fe80f1bfd86425cad5552a80030ed44ee, reversing
    changes made to 60ae83684e844d31320a19ff9e62e1ba835cbef0.

--- final/src/app/warehouse/product/page.tsx
+++
@@ -1,8 +0,0 @@
-function ProductPage() {
-  return (
-    <div className="flex h-full w-full items-center justify-center rounded-b-xl border-2">
-      <div></div>
-    </div>
-  );
-}
-export default ProductPage;
